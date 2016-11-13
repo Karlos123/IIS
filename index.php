a8e766a8e1092a0747571ca8974091d3df0d32e7
karlos123 <kareljiranek@seznam.cz>
2016-11-13 13:49:48 +0100

    First

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+    //session_save_path("tmp/");
+    session_start();
+?>
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<!-- HEAD  -->
+<head>
+<meta  charset=utf-8>
+<title>ZOOIS</title>
+<link rel="stylesheet" href="style.css" type="text/css" >
+</head>
+
+
+<!-- BODY  -->
+<body>
+
+  <!-- Logo  -->
+  <img src="elephant.png" alt="Logo" style="height:100px;">
+
+  <div id="sidebar"></div>
+  <div id="header"></div>
+  <div id="content"></div>
+
+  <!-- Side bar -->
+  <ul>
+   <li><a href="default.asp">Home</a></li>
+   <li><a href="default.asp">Personal schedule</a></li>
+   <li><a href="default.asp">Planning</a></li>
+   <li><a href="runs.asp">Animals</a></li>
+   <li><a href="news.asp">Runs</a></li>
+   <li><a href="news.asp">News</a></li>
+  </ul>
+
+
+
+</div>
+
+
+
+
+</body>
+
+
+</html>
