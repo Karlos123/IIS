365c9a7cb48017382b58aeb45f8b8716f4528baa
Frantisek Kropac <kropac.f@windowslive.com>
2016-11-23 00:58:35 +0100

    Uprava DB

--- xjiran00_xkropa06.sql
+++ xjiran00_xkropa06.sql
@@ -5,6 +5,7 @@
 -- Datum: 12. 3. 2015
 -- Zadani: zoologicka zahrada
 
+SET NAMES 'utf8';
 
 drop table if exists Osetrovatel CASCADE;
 drop table if exists Vybeh CASCADE;
@@ -27,16 +28,183 @@ drop table if exists Hlavni_Osetrovatel  CASCADE;
 -- Osetrovatel
 CREATE TABLE Osetrovatel (
   os_cislo SMALLINT NOT NULL AUTO_INCREMENT,
-  jmeno VARCHAR(20),
-  primeni VARCHAR(20),
-  datum_narozeni DATE,
-  mesto VARCHAR(30),
-  ulice VARCHAR(20),
-  nej_dosazene_vzdelani VARCHAR(20),
-
-
-  CONSTRAINT PK_os_cislo PRIMARY KEY (os_cislo)
+  jmeno VARCHAR(20) NOT NULL,
+  primeni VARCHAR(20) NOT NULL,
+  datum_narozeni DATE DEFAULT NULL,
+  mesto VARCHAR(30) DEFAULT NULL,
+  ulice VARCHAR(20) DEFAULT  NULL,
+  nej_dosazene_vzdelani VARCHAR(20) DEFAULT NULL,
+  PRIMARY KEY(os_cislo)
 );
+
+CREATE TABLE Uzivatel (
+  pk SMALLINT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(40) UNIQUE NOT NULL,
+  password VARCHAR(40) NOT NULL,
+  privileges SMALLINT NOT NULL,
+  pk_osetrovatel SMALLINT,
+  PRIMARY KEY(pk)
+);
+
+CREATE TABLE Hlavni_Osetrovatel (
+  os_cislo SMALLINT
+);
+
+CREATE TABLE Skoleni (
+  os_cislo SMALLINT,
+  jmeno_skoleni VARCHAR(60),
+  PRIMARY KEY (os_cislo, jmeno_skoleni)
+);
+
+CREATE TABLE Vybeh (
+  cislo_vybehu SMALLINT NOT NULL AUTO_INCREMENT,
+  typ VARCHAR(50) NOT NULL,
+  max_zvirat SMALLINT DEFAULT '1',
+  cas_na_cisteni INT DEFAULT '10', -- minut
+  otevren DATE DEFAULT '2000-10-10',
+  PRIMARY KEY (cislo_vybehu)
+);
+
+CREATE TABLE Pomucky (
+  cislo_vybehu SMALLINT,
+  jmeno_pomucky VARCHAR(20),
+  PRIMARY KEY (cislo_vybehu, jmeno_pomucky)
+);
+
+CREATE TABLE Opravy (
+  cislo_vybehu SMALLINT,
+  ev_opravy VARCHAR(20) DEFAULT 'oprava1', -- evdence opravy
+  ucel_opravy VARCHAR(100) NOT NULL,
+  datum_opravy DATE NOT NULL,
+  PRIMARY KEY (cislo_vybehu, ev_opravy)
+);
+
+CREATE TABLE Zvire (
+  kod_zvirete INTEGER NOT NULL AUTO_INCREMENT,
+  jmeno VARCHAR(20) NOT NULL,
+  pohlavi VARCHAR(10) DEFAULT NULL,
+  zarazeni VARCHAR(50) NOT NULL,
+  hmotnost DECIMAL(8,3) DEFAULT NULL, -- kilo
+  datum_narozeni DATE DEFAULT NULL,
+  datum_umrti DATE DEFAULT NULL,
+  matka VARCHAR(20) DEFAULT NULL,
+  otec VARCHAR (20) DEFAULT NULL,
+  zdarvotni_stav VARCHAR(20) DEFAULT NULL,
+  pos_zdrav_prohlidka DATE DEFAULT NULL,
+  cislo_vybehu SMALLINT DEFAULT NULL,
+  PRIMARY KEY (kod_zvirete)
+);
+
+CREATE TABLE Cizi_zoo (
+  jmeno_zoo  VARCHAR(50) NOT NULL,
+  mesto VARCHAR(30) DEFAULT 'Brno',
+  ulice VARCHAR(20) DEFAULT 'Božetěchova 2',
+  ICO NUMERIC(8,0) DEFAULT NULL,
+  DIC NUMERIC(15,0) DEFAULT NULL,
+  poverena_osoba VARCHAR(20) DEFAULT 'Petr Petrů',
+  PRIMARY KEY(jmeno_zoo)
+);
+
+CREATE TABLE Prispivatel (
+  cislo_prispivatele SMALLINT NOT NULL AUTO_INCREMENT,
+  jmeno VARCHAR(30) DEFAULT NULL
+  typ_prispivani VARCHAR(30) DEFAULT NULL,
+  mesto VARCHAR(30) DEFAULT NULL,
+  ulice VARCHAR(20) DEFAULT NULL,
+  castka INTEGER DEFAULT NULL,
+  celkova_castka INTEGER DEFAULT NULL,
+  PRIMARY KEY(cislo_prispivatele)
+);
+
+CREATE TABLE Stara_se (
+    os_cislo SMALLINT,
+    kod_zvirete INTEGER,
+    PRIMARY KEY(os_cislo, kod_zvirete)
+);
+
+CREATE TABLE Cisti (
+    os_cislo SMALLINT,
+    cislo_vybehu SMALLINT,
+    datum_cisteni date,
+    od DATE NOT NULL,
+    do DATE NOT NULL,
+    stav_po VARCHAR(30),
+    PRIMARY KEY(os_cislo, cislo_vybehu)
+);
+
+CREATE TABLE Krmi (
+    os_cislo SMALLINT,
+    kod_zvirete INT,
+    datum_krmeni DATE NOT NULL,
+    od TIME NOT NULL,
+    do TIME NOT NULL,
+    mnozstvi_zradla DECIMAL(8,3) NOT NULL CHECK (mnozstvi_zradla > 0), -- Kg
+    PRIMARY KEY(os_cislo, kod_zvirete, datum_krmeni, od)
+);
+
+CREATE TABLE Prispiva (
+    cislo_prispivatele SMALLINT,
+    kod_zvirete INT,
+    PRIMARY KEY(cislo_prispivatele, kod_zvirete)
+);
+
+CREATE TABLE Zapujcuje (
+    jmeno_zoo  VARCHAR(50),
+    kod_zvirete INT,
+    od DATE NOT NULL,
+    do DATE,
+    ucel_zapujceni VARCHAR(80),
+    PRIMARY KEY(jmeno_zoo, kod_zvirete)
+);
+
+
+ALTER TABLE Uzivatel ADD CONSTRAINT FK_uzivatel FOREIGN KEY (pk_osetrovatel)
+REFERENCES Osetrovatel(os_cislo) ON DELETE CASCADE;
+
+ALTER TABLE Hlavni_Osetrovatel ADD CONSTRAINT FK_Hlavni_O_nadrizeny FOREIGN KEY (os_cislo)
+REFERENCES Osetrovatel(os_cislo) ON DELETE CASCADE;
+
+ALTER TABLE Skoleni ADD CONSTRAINT FK_os_cislo_osetrovatel FOREIGN KEY (os_cislo)
+REFERENCES Osetrovatel(os_cislo) ON DELETE CASCADE;
+
+ALTER TABLE Pomucky ADD CONSTRAINT FK_cislo_vybehu FOREIGN KEY (cislo_vybehu)
+REFERENCES Vybeh (cislo_vybehu) ON DELETE CASCADE;
+
+ALTER TABLE Opravy ADD CONSTRAINT FK_opravy_vybeh FOREIGN KEY (cislo_vybehu)
+REFERENCES Vybeh (cislo_vybehu) ON DELETE CASCADE;
+
+ALTER TABLE Zvire ADD CONSTRAINT FK_Vybeh FOREIGN KEY (cislo_vybehu)
+REFERENCES Vybeh (cislo_vybehu) ON DELETE CASCADE;
+
+ALTER TABLE Stara_se ADD CONSTRAINT FK_Osetrovatel_Stara_se FOREIGN KEY (os_cislo)
+REFERENCES Osetrovatel (os_cislo) ON DELETE CASCADE;
+
+ALTER TABLE Stara_se ADD CONSTRAINT FK_Osetrovatel_Skoleni FOREIGN KEY (kod_zvirete)
+REFERENCES Zvire (kod_zvirete) ON DELETE CASCADE;
+
+ALTER TABLE Cisti ADD CONSTRAINT FK_Stara_os_cislo FOREIGN KEY (os_cislo)
+REFERENCES Osetrovatel (os_cislo) ON DELETE CASCADE;
+
+ALTER TABLE Cisti ADD CONSTRAINT FK_Stara_se_cislo_vybehu FOREIGN KEY (cislo_vybehu)
+REFERENCES Vybeh (cislo_vybehu) ON DELETE CASCADE;
+
+ALTER TABLE Krmi ADD CONSTRAINT FK_Krmi_os_cislo FOREIGN KEY (os_cislo)
+REFERENCES Osetrovatel (os_cislo) ON DELETE CASCADE;
+
+ALTER TABLE Krmi ADD CONSTRAINT FK_Krmni_kod_zvirete FOREIGN KEY (kod_zvirete)
+REFERENCES Zvire (kod_zvirete) ON DELETE CASCADE;
+
+ALTER TABLE Prispiva ADD CONSTRAINT FK_Prispiva_cislo_prispivatele FOREIGN KEY (cislo_prispivatele)
+REFERENCES Prispivatel(cislo_prispivatele) ON DELETE CASCADE;
+
+ALTER TABLE Prispiva ADD CONSTRAINT FK_Prispiva_kod_zvirete FOREIGN KEY (kod_zvirete)
+REFERENCES Zvire(kod_zvirete) ON DELETE CASCADE;
+
+ALTER TABLE Zapujcuje ADD CONSTRAINT FK_Zapujcuje_jmeno_zoo FOREIGN KEY (jmeno_zoo)
+REFERENCES Cizi_zoo (jmeno_zoo) ON DELETE CASCADE;
+
+ALTER TABLE Zapujcuje ADD CONSTRAINT FK_Zapujcuje_kod_zvirete FOREIGN KEY (kod_zvirete)
+REFERENCES Zvire(kod_zvirete) ON DELETE CASCADE;
 
 INSERT INTO Osetrovatel VALUES (NULL, 'Antonín', 'Novotný', '1965-02-15', 'Praha', 'Nová 957', 'Vysokoškolské' );
 INSERT INTO Osetrovatel VALUES (NULL, 'Josef', 'Vomáčka', '1980-12-30', 'Brno', 'Cejl 8', 'Vysokoškolské' );
@@ -46,51 +214,15 @@ INSERT INTO Osetrovatel VALUES (NULL, 'Gabriela', 'Soukalová', '1989-11-01' , '
 INSERT INTO Osetrovatel VALUES (NULL, 'Martina', 'Záblová', '1988-07-31' , 'Louny', 'Nová 199', 'Vysokoškolské');
 INSERT INTO Osetrovatel VALUES (NULL, 'Martin', 'Jásal', '1990-10-31' , 'Brno', 'Vánoční 199', 'Vysokoškolské');
 
-CREATE TABLE Uzivatel (
-  pk SMALLINT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(40) UNIQUE NOT NULL,
-  password VARCHAR(40) NOT NULL,
-  privileges SMALLINT NOT NULL,
-  pk_osetrovatel SMALLINT,
-
-  CONSTRAINT PK_pk PRIMARY KEY (pk),
-
-  CONSTRAINT FK_uzivatel FOREIGN KEY (pk_osetrovatel)
-  REFERENCES Osetrovatel(os_cislo)
-);
 
 INSERT INTO Uzivatel VALUES (NULL, 'ant', '1', '1', (SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 1));
 INSERT INTO Uzivatel VALUES (NULL, 'jos', '1', '2', (SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 2));
 INSERT INTO Uzivatel VALUES (NULL, 'admin', 'admin', '3', null);
 
 
-
-
--- HLavni osetrovatel
-CREATE TABLE Hlavni_Osetrovatel (
-  os_cislo SMALLINT,
-
-
-  CONSTRAINT FK_Hlavni_O_nadrizeny
-      FOREIGN KEY (os_cislo)
-      REFERENCES Osetrovatel (os_cislo)
-);
-
 INSERT INTO Hlavni_Osetrovatel VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 2));
 
-
 -- Skoleni
-CREATE TABLE Skoleni (
-  os_cislo SMALLINT,
-  jmeno_skoleni VARCHAR(60),
-
-  CONSTRAINT PK_os_cislo_jmeno_skoleni PRIMARY KEY(os_cislo, jmeno_skoleni),
-
-  CONSTRAINT FK_os_cislo_osetrovatel
-      FOREIGN KEY (os_cislo)
-      REFERENCES Osetrovatel (os_cislo)
-
-);
 
 INSERT INTO Skoleni VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 1), 'Krmení plachých lesních dravců');
 INSERT INTO Skoleni VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 3), 'Čištěná těžko dostupných prostorů');
@@ -105,17 +237,7 @@ INSERT INTO Skoleni VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 6
 
 /**********************   Vybeh   **********************/
 -- Vybeh
-CREATE TABLE Vybeh (
-  cislo_vybehu SMALLINT NOT NULL AUTO_INCREMENT,
-  typ VARCHAR(50) NOT NULL,
-  max_zvirat SMALLINT,
-  cas_na_cisteni INT, -- minut
-  otevren DATE,
-
-  CONSTRAINT PK_cislo_vybehu PRIMARY KEY (cislo_vybehu)
-);
-
-INSERT INTO Vybeh VALUES (NULL, 'Aqarium', 10, 30, '2020-10-15');
+INSERT INTO Vybeh VALUES (NULL, 'Aqarium', 10, 30, '2010-10-15');
 INSERT INTO Vybeh VALUES (NULL, 'Savana',   3, 60, '1999-09-08');
 INSERT INTO Vybeh VALUES (NULL, 'Arktik',  5, 20, '2001-12-17' );
 INSERT INTO Vybeh VALUES (NULL, 'Terarium', NULL , 30, NULL);
@@ -125,19 +247,6 @@ INSERT INTO Vybeh VALUES (NULL, 'Tajga', 3, 20, '2013-06-26');
 INSERT INTO Vybeh VALUES (NULL, 'Vodní nádrž', 3, 20, '2015-08-15');
 
 -- Pomucky
-CREATE TABLE Pomucky (
-  cislo_vybehu SMALLINT,
-  jmeno_pomucky VARCHAR(20),
-
-  CONSTRAINT PK_jmeno_pomucky PRIMARY KEY (cislo_vybehu, jmeno_pomucky),
-
-  CONSTRAINT FK_cislo_vybehu
-      FOREIGN KEY (cislo_vybehu)
-      REFERENCES Vybeh (cislo_vybehu)
-
-);
-
-
 INSERT INTO Pomucky VALUES ((SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 1), 'Kyblik 4,5 L');
 INSERT INTO Pomucky VALUES ((SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 1), 'Houba velká') ;
 INSERT INTO Pomucky VALUES ((SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 1), 'Saponát');
@@ -152,50 +261,12 @@ INSERT INTO Pomucky VALUES ((SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu =
 INSERT INTO Pomucky VALUES ((SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 6), 'Koš na odpadky');
 
 -- Opravy
-CREATE TABLE Opravy (
-  cislo_vybehu SMALLINT,
-  ev_opravy VARCHAR(20), -- evdence opravy
-  ucel_opravy VARCHAR(100) NOT NULL,
-  datum_opravy DATE NOT NULL,
-
-  CONSTRAINT PK_cislo_opravy PRIMARY KEY (cislo_vybehu, ev_opravy),
-
-  CONSTRAINT FK_opravy_vybeh
-      FOREIGN KEY (cislo_vybehu)
-      REFERENCES Vybeh (cislo_vybehu)
-
-
-);
-
 INSERT INTO Opravy VALUES ((SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 1), '1aqa0001', 'Vadné těsnění fitrace', '2000-10-16');
 INSERT INTO Opravy VALUES ((SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 2), '2sava0001', 'Vyměna porušeného plotu v sekci 20', '2010-05-20');
 
 
-
 /**********************  Zvire ****************************/
 -- Zvire
-CREATE TABLE Zvire (
-  kod_zvirete INTEGER NOT NULL AUTO_INCREMENT,
-  jmeno VARCHAR(20),
-  pohlavi VARCHAR(10) CHECK (pohlavi = 'female' OR pohlavi = 'male' OR pohlavi = 'N/A' or pohlavi = 'hermafrodit'),
-  zarazeni VARCHAR(50) NOT NULL,
-  hmotnost DECIMAL(8,3) CHECK (hmotnost > 0), -- kilo
-  datum_narozeni DATE,
-  datum_umrti DATE,
-  matka VARCHAR(20),
-  otec VARCHAR (20),
-  zdarvotni_stav VARCHAR(20) NOT NULL CHECK(zdarvotni_stav = 'OK' OR zdarvotni_stav = 'Nemocný' OR zdarvotni_stav = 'N/A'),
-  pos_zdrav_prohlidka DATE,
-  cislo_vybehu SMALLINT,
-
-  CONSTRAINT PK_kod_zvirete PRIMARY KEY (kod_zvirete),
-
-  CONSTRAINT FK_Vybeh
-      FOREIGN KEY (cislo_vybehu)
-      REFERENCES Vybeh(cislo_vybehu)
-
-);
-
 INSERT INTO Zvire VALUES (NULL, 'Alex', 'male', 'Kočkovité šelmy - Lvi', 150, '2000-01-20', NULL, 'Ala', 'Alan', 'OK', '2010-01-19',
   (SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 2));
 INSERT INTO Zvire VALUES (NULL, 'Lila', 'female', 'Kočkovité šelmy - Lvi', 120, '2002-01-15', NULL, 'Ela', 'Elan', 'OK', '2012-06-22',
@@ -219,59 +290,20 @@ INSERT INTO Zvire VALUES (NULL, 'Barbara', 'female', 'Lachtan hřivnatý', 250, 
   (SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 8));
 
 
-
 -- Cizi ZOO
-CREATE TABLE Cizi_zoo (
-  jmeno_zoo  VARCHAR(50) NOT NULL,
-  mesto VARCHAR(30) NOT NULL,
-  ulice VARCHAR(20) NOT NULL,
-  ICO NUMERIC(8,0),
-  DIC NUMERIC(15,0),
-  poverena_osoba VARCHAR(20) NOT NULL,
-
-  CONSTRAINT PK_jmeno_zoo PRIMARY KEY(jmeno_zoo)
-);
-
 INSERT INTO Cizi_zoo VALUES ('ZOO Hradec Králové', 'Hradec Králové', 'Zologická 12', 23465735, 574936255967153, 'Aleš Smutný');
 INSERT INTO Cizi_zoo VALUES ('ZOO Praha', 'Praha', 'Trója 132', 85649323, 928374654738291, 'Petr Spálený');
 INSERT INTO Cizi_zoo VALUES ('NYC ZOO', 'New York City', 'Manhattan 150', 0, 0, 'Anny Whitestone');
 
 -- Prispivatel
 
-CREATE TABLE Prispivatel (
-  cislo_prispivatele SMALLINT NOT NULL AUTO_INCREMENT,
-  typ_prispivani VARCHAR(30) NOT NULL CHECK (typ_prispivani = 'Měsíční' OR typ_prispivani = 'Jednorázový' OR typ_prispivani = 'Roční'),
-  mesto VARCHAR(30) NOT NULL,
-  ulice VARCHAR(20) NOT NULL,
-  castka INTEGER NOT NULL,
-  celkova_castka INTEGER NOT NULL,
-
-  CONSTRAINT PK_cislo_prispivatele PRIMARY KEY(cislo_prispivatele)
-
-);
-
-INSERT INTO Prispivatel VALUES (NULL, 'Měsíční', 'Brno', 'Cejl 12', 10000, 10000);
-INSERT INTO Prispivatel VALUES (NULL, 'Jednorázový', 'Praha', 'Brněnská 13', 20000, 20000);
+INSERT INTO Prispivatel VALUES (NULL, "Dopravní podnik města Brna", 'Měsíční', 'Brno', 'Cejl 12', 10000, 10000);
+INSERT INTO Prispivatel VALUES (NULL, 'Jednorázový', "Dopravní podnik hlavního města Prahy",  'Praha', 'Brněnská 13', 20000, 20000);
 
 /*** VZTAHY ***/
 
 
 -- Stara se (o zvire)
-CREATE TABLE Stara_se (
-    os_cislo SMALLINT,
-    kod_zvirete INTEGER,
-    CONSTRAINT PK_os_cislo_kod_zvirete PRIMARY KEY(os_cislo, kod_zvirete),
-
-    CONSTRAINT FK_Osetrovatel_Stara_se
-      FOREIGN KEY (os_cislo)
-      REFERENCES Osetrovatel (os_cislo),
-
-    CONSTRAINT FK_Osetrovatel_Skoleni
-      FOREIGN KEY (kod_zvirete)
-      REFERENCES Zvire(kod_zvirete)
-
-);
-
 INSERT INTO Stara_se VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 1), (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 1));
 INSERT INTO Stara_se VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 2), (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 2));
 INSERT INTO Stara_se VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 3), (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 3));
@@ -283,24 +315,8 @@ INSERT INTO Stara_se VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 
 INSERT INTO Stara_se VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 3), (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 8));
 
 -- Cisti
-CREATE TABLE Cisti (
-    os_cislo SMALLINT,
-    cislo_vybehu SMALLINT,
-    datum_cisteni date,
-    od DATE NOT NULL,
-    do DATE NOT NULL,
-    stav_po VARCHAR(30),
-    CONSTRAINT PK_Cisti_os_cislo_cislo_vybehu PRIMARY KEY(os_cislo, cislo_vybehu),
 
-    CONSTRAINT FK_Stara_os_cislo
-      FOREIGN KEY (os_cislo)
-      REFERENCES Osetrovatel (os_cislo),
 
-    CONSTRAINT FK_Stara_se_cislo_vybehu
-      FOREIGN KEY (cislo_vybehu)
-      REFERENCES Vybeh(cislo_vybehu)
-
-);
 
 INSERT INTO Cisti VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 4),
                           (SELECT cislo_vybehu FROM Vybeh WHERE cislo_vybehu = 1),
@@ -329,24 +345,7 @@ INSERT INTO Cisti VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 6),
 
 
 -- Krmi
-CREATE TABLE Krmi (
-    os_cislo SMALLINT,
-    kod_zvirete INT,
-    datum_krmeni DATE NOT NULL,
-    od TIME NOT NULL,
-    do TIME NOT NULL,
-    mnozstvi_zradla DECIMAL(8,3) NOT NULL CHECK (mnozstvi_zradla > 0), -- Kg
-    CONSTRAINT PK_krmi_os_cislo_kod_zvirete PRIMARY KEY(os_cislo, kod_zvirete, datum_krmeni, od),
 
-    CONSTRAINT FK_Krmi_os_cislo
-      FOREIGN KEY (os_cislo)
-      REFERENCES Osetrovatel (os_cislo),
-
-    CONSTRAINT FK_Krmni_kod_zvirete
-      FOREIGN KEY (kod_zvirete)
-      REFERENCES Zvire(kod_zvirete)
-
-);
 
 INSERT INTO Krmi VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 1),
                          (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 1),
@@ -394,21 +393,7 @@ INSERT INTO Krmi VALUES ((SELECT os_cislo FROM Osetrovatel WHERE os_cislo = 6),
 
 
 -- Prispiva
-CREATE TABLE Prispiva (
-    cislo_prispivatele SMALLINT,
-    kod_zvirete INT,
 
-    CONSTRAINT PK_Prispiva_CP_KZ PRIMARY KEY(cislo_prispivatele, kod_zvirete),
-
-    CONSTRAINT FK_Prispiva_cislo_prispivatele
-      FOREIGN KEY (cislo_prispivatele)
-      REFERENCES Prispivatel(cislo_prispivatele),
-
-    CONSTRAINT FK_Prispiva_kod_zvirete
-      FOREIGN KEY (kod_zvirete)
-      REFERENCES Zvire(kod_zvirete)
-
-);
 
 INSERT INTO Prispiva VALUES ((SELECT cislo_prispivatele FROM Prispivatel WHERE cislo_prispivatele = 1), (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 1));
 INSERT INTO Prispiva VALUES ((SELECT cislo_prispivatele FROM Prispivatel WHERE cislo_prispivatele = 1), (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 2));
@@ -419,23 +404,20 @@ INSERT INTO Prispiva VALUES ((SELECT cislo_prispivatele FROM Prispivatel WHERE c
 
 
 -- Zapujcuje
-CREATE TABLE Zapujcuje (
-    jmeno_zoo  VARCHAR(50),
-    kod_zvirete INT,
-    od DATE NOT NULL,
-    do DATE,
-    ucel_zapujceni VARCHAR(80),
-    CONSTRAINT PK_Zapujcuje_JZ_KZ PRIMARY KEY(jmeno_zoo, kod_zvirete),
 
-    CONSTRAINT FK_Zapujcuje_jmeno_zoo
-      FOREIGN KEY (jmeno_zoo)
-      REFERENCES Cizi_zoo (jmeno_zoo),
-
-    CONSTRAINT FK_Zapujcuje_kod_zvirete
-      FOREIGN KEY (kod_zvirete)
-      REFERENCES Zvire(kod_zvirete)
-
-);
 
 INSERT INTO Zapujcuje VALUES ('ZOO Hradec Králové', (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 1), '2015-09-01', NULL, 'Rozmnožování');
 INSERT INTO Zapujcuje VALUES ('ZOO Praha', (SELECT kod_zvirete FROM Zvire WHERE kod_zvirete = 2), '2015-10-01', NULL, 'Rozmnožování');
+
+
+-- delimiter $$
+-- CREATE TRIGGER vybeh_delete BEFORE DELETE ON Vybeh
+-- FOR EACH ROW
+-- BEGIN
+-- UPDATE Zvire SET Zvire.cislo_vybehu=NULL;
+-- UPDATE Pomucky SET Pomucky.cislo_vybehu=NULL;
+-- UPDATE Cisti SET Cisti.cislo_vybehu=NULL;
+-- UPDATE Opravy SET Opravy.cislo_vybehu=NULL;
+-- END
+-- $$
+-- delimiter ;
